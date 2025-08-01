ce3e33de79369b8f747582a1c94e5dbdcbc1a9f6
ecesueryigit <ecesueryigit@gmail.com>
2025-08-01 22:48:25 +0300

    Delete 03.py

--- 03.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-folder_path= input("Listelemek istediğiniz klasör yolunu girin: ")
-
-
-result= subprocess.run(["ls", folder_path], capture_output=True, text=True)
-
-print("Output: " , result.stdout)
-print("Error: ", result.stderr)
